d68baa0cae3f54622f1b0cbf68fa9188fb241567
pa1nkilerr <129454773+pa1nkilerr@users.noreply.github.com>
2023-11-03 20:50:31 -0500

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,16 @@
+package inputCalculations;
+
+public class AreaRectangle {
+	
+	public static double CalAreaRec(double width, double height) {
+		return width*height;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("The area of the rectangle is " + CalAreaRec(3,5));
+		
+	}
+
+}
